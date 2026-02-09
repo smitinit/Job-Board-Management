2d376a557e17bbfa76a9158b29fc795b06fb6193
smitinit <patelsmit090305@gmail.com>
2026-02-09 11:48:58 +0530

    updated skleton

--- app/loading.tsx
+++ app/loading.tsx
@@ -6,7 +6,7 @@ export default function Loading() {
     <>
       <div className="sticky top-0 z-50 grid grid-cols-2 gap-2 bg-background px-4 py-2">
         <Skeleton className="h-10 w-full rounded-md" />
-        <Skeleton className="h-10 w-full rounded-md sm:w-[160px]" />
+        <Skeleton className="h-10 w-full rounded-md sm:w-40" />
       </div>
 
       <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3">
